c72130db34ba5873bb225dbccd57196b087d0413
一丝 <percyley@qq.com>
2022-03-16 17:46:39 +0800

    chore: generate the sample SVG file in example

--- examples/basic.js
+++ examples/basic.js
@@ -1,14 +1,21 @@
-const { readFileSync } = require('fs')
-const path = require('path')
+const { Buffer } = require('buffer')
+const { readFileSync, writeFileSync } = require('fs')
+const { join } = require('path')
 
 const { FontKit } = require('../wasm-node')
 
-const FONT_PATH = path.resolve(__dirname, 'OpenSans-Italic.ttf')
+const FONT_PATH = join(__dirname, 'OpenSans-Italic.ttf')
 
 const data = readFileSync(FONT_PATH)
 const fontkit = new FontKit()
 const key = fontkit.add_font_from_buffer(data)
 const font = fontkit.query(key)
-
+const svgPath = font.glyph_path('A').to_string()
 console.info('em-box = ', font.units_per_em)
-console.info(font.glyph_path('A').to_string())
+
+const svg = `<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <path d="${svgPath}" fill="blue" />
+</svg>
+`
+writeFileSync(join(__dirname, './out.svg'), Buffer.from(svg))
+console.info(svg)
