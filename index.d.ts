5d903cef9d9de659a613983968db7862620e4388
zhuoxian.dzx <zhuoxian.dzx@alibaba-inc.com>
2024-06-06 16:27:49 +0800

    feat: add readme field in Cargo.toml

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,11 @@
-export { FontKit, Font, FontInfo, FontKey } from './pkg/interfaces/alibaba-fontkit-fontkit-interface';
+import {
+  FontKit,
+  Font,
+  FontInfo,
+  FontKey,
+  AlibabaFontkitFontkitInterface,
+} from './pkg/interfaces/alibaba-fontkit-fontkit-interface';
+
+export import strWidthToNumber = AlibabaFontkitFontkitInterface.strWidthToNumber;
+export import numberWidthToStr = AlibabaFontkitFontkitInterface.numberWidthToStr;
+export { FontKit, Font, FontInfo, FontKey };
